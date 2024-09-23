abc515615cc56000917055819cbe651855b98802
Nur Arini <nurarini0302@gmail.com>
2024-09-23 17:23:42 +0700

    [UPDATE] up ui

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,15 +4,15 @@ export default function Skills(){
     <>
       <div className="section section-2 pt-10 pb-8">
         <h1 className="font-bold text-3xl">💻Tech & Tools</h1>
-        <div className="flex flex-wrap gap-4 justify-center sm:justify-start">
-          {
-            logos.map((el,index) => (
-              <div key={index} className="p-5 bg-slate-800 backdrop-blur-sm rounded-[10px] hover:border hover:border-slate-500">
-                <img src={`./img/${el}.png`} alt={`${el}`} className="w-[50px] h-[50px]"/>
-              </div>
-            ))
-          }
-        </div>
+          <div className="flex flex-wrap gap-4 justify-center sm:justify-start">
+            {
+              logos.map((el,index) => (
+                <div key={index} className="p-5 bg-slate-800 backdrop-blur-sm rounded-[10px] hover:border hover:border-slate-500">
+                  <img src={`./img/${el}.png`} alt={`${el}`} className="w-[50px] h-[50px]"/>
+                </div>
+              ))
+            }
+          </div>
       </div>
     </>
   )
